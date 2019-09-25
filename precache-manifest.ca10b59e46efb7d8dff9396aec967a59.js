fcefd3b1c1641f7e68a6bd40f27ac0d7156c244b
vikashAppBrewery <vikash99318@live.com>
2019-09-25 17:34:47 +0530

    Updates

---
+++ precache-manifest.ca10b59e46efb7d8dff9396aec967a59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5795f97f05f52416e282b6cb995b920e",
+    "url": "/cat-database/index.html"
+  },
+  {
+    "revision": "337ac6b6d4d3a9fe3abe",
+    "url": "/cat-database/static/css/main.8b9cc6ac.chunk.css"
+  },
+  {
+    "revision": "dce70a754d2ea0e0e967",
+    "url": "/cat-database/static/js/2.0aab78e1.chunk.js"
+  },
+  {
+    "revision": "337ac6b6d4d3a9fe3abe",
+    "url": "/cat-database/static/js/main.bd163724.chunk.js"
+  },
+  {
+    "revision": "3db7aac6ac33696d71e5",
+    "url": "/cat-database/static/js/runtime-main.03ec84bc.js"
+  }
+]);
